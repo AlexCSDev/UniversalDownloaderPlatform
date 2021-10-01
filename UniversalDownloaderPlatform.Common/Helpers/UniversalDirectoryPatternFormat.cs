6e1390560b4c582ab067424c53b06009046ac51b
Miguel Therriault <phoenixthered@hotmail.com>
2021-10-01 11:55:38 -0400

    Missing file

---
+++ UniversalDownloaderPlatform.Common/Helpers/UniversalDirectoryPatternFormat.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace UniversalDownloaderPlatform.Common.Helpers
+{
+    public class UniversalDirectoryPatternFormat
+    {
+        public static readonly char[] InvalidPathChars;
+
+        static UniversalDirectoryPatternFormat()
+        {
+            InvalidPathChars = Path.GetInvalidPathChars().Concat("\\/:*?\"<>|".ToCharArray()).ToArray();
+        }
+    }
+}
